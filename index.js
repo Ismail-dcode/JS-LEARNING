f7ff928d12fb3597605a766a74c765577eeeda40
Ismail-dcode <ismailpromt2i@gmail.com>
2024-11-16 22:04:27 +0530

    math.thing

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(5,9,8,4));
+
+
+console.log(Math.random());
+
+console.log(Math.random()*6);
+
+console.log(Math.floor(Math.random()*6)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+ 
+
