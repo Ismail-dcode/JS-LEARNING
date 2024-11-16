afc29865d515411afdb4fb2e70177b42d9b565e2
Ismail-dcode <ismailpromt2i@gmail.com>
2024-11-16 22:13:56 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(Math.random()*6);
 console.log(Math.floor(Math.random()*6)+1);
 
 const min = 10
-const max = 20
+const max = 30
 
 console.log(Math.floor(Math.random()*(max-min +1))+min);
  
