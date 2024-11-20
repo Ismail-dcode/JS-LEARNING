093502a9188a6455ae05fdc9cdca0ac675232b15
Ismail S <ismailpromt2i@gmail.com>
2024-11-20 15:35:34 +0530

    learning

---
+++ index.js
@@ -0,0 +1,10 @@
+const instaUser = {}
+
+instaUser.id= "Y2i"
+instaUser.name= "jarvis-2i"
+instaUser.email = "jarvis@gmail.com "
+
+const obj1 = Object.entries(instaUser);
+console.log(instaUser);
+
+
