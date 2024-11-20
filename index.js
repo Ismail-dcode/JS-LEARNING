3c18ce44d331d4ca1a767375ef85c5a9b70a0b1d
Ismail-dcode <ismailpromt2i@gmail.com>
2024-11-20 15:53:54 +0530

    new test

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const instaUser = {}
 instaUser.id= "X2i"
 instaUser.name= "jarvis-2i"
 instaUser.email = "jarvis@gmail.com "
+instaUser.id= "Y2i"
+instaUser.name= "lucifer-2i"
+instaUser.email = "lucifer@gmail.com "
 
 const obj1 = Object.entries(instaUser);
 console.log(instaUser);
